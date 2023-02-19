7cd63bb891373e0f3bfbea62b4ae207082598677
judgingalready <judgingalready@golang.com>
2023-02-19 15:20:46 +0800

    gorm

---
+++ simple-demo-main/controller/feed.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type FeedResponse struct {
+	Response
+	VideoList []Video `json:"video_list,omitempty"`
+	NextTime  int64   `json:"next_time,omitempty"`
+}
+
+// Feed same demo video list for every request
+func Feed(c *gin.Context) {
+	dsn := "root:Qwer1234!@tcp(localhost:3306)/feed?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(db, err)
+	}
+
+	c.JSON(http.StatusOK, FeedResponse{
+		Response:  Response{StatusCode: 0},
+		VideoList: DemoVideos,
+		NextTime:  time.Now().Unix(),
+	})
+}
