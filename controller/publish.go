798f121c1c7f3a53dc009ad3ad620d694657d3cf
judgingalready <judgingalready@golang.com>
2023-02-22 11:27:49 +0800

    feat:视频截帧完成

--- controller/publish.go
+++ controller/publish.go
@@ -1,13 +1,18 @@
 package controller
 
 import (
+	"bytes"
 	"fmt"
+	// "io"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strconv"
 	"time"
 
+	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 type VideoListResponse struct {
@@ -38,28 +43,62 @@ func Publish(c *gin.Context) {
 	}
 
 	filename := filepath.Base(data.Filename)
+	// 视频保存的本地路径：用户Id_时间戳_filename.mp4
 	currentTime := time.Now().Unix()
+	filename = fmt.Sprintf("%d_%d_%s", user.Id, currentTime, filename)
 	// 视频存入Videos数据库的url：IP:Port/static/视频本地路径
 	ip_port := "114.212.85.230:8080" //暂时写死
-	filename = fmt.Sprintf("%d_%d_%s", user.Id, currentTime, filename)
-	finalName := fmt.Sprintf("%s%s", "http://"+ip_port+"/static/", filename)
-	// 视频保存的本地路径：用户Id_视频描述_时间戳
+	videoName := fmt.Sprintf("%s%s", "http://"+ip_port+"/static/", filename)
 	saveFile := filepath.Join("./public/", filename)
 
-	CreateVideoErr := db.Create(&Video{AuthorID: user.Id, PlayUrl: finalName}).Error
-	if CreateVideoErr != nil {
+	//保存视频到本地
+	if err := c.SaveUploadedFile(data, saveFile); err != nil {
 		c.JSON(http.StatusOK, Response{
 			StatusCode: 1,
-			StatusMsg:  CreateVideoErr.Error(),
+			StatusMsg:  err.Error(),
 		})
 		return
 	}
 
-	if err := c.SaveUploadedFile(data, saveFile); err != nil {
-		c.SaveUploadedFile(data, saveFile)
+	//获取视频封面
+	frameNum := 1
+	covelBuf := bytes.NewBuffer(nil)
+	err = ffmpeg.Input(saveFile).
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(covelBuf, os.Stdout).
+		Run()
+	if err != nil {
 		c.JSON(http.StatusOK, Response{
 			StatusCode: 1,
 			StatusMsg:  err.Error(),
+		})
+		return
+	}
+	//保存封面到本地
+	covelImage, err := imaging.Decode(covelBuf)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
+	covelName := saveFile[:len(saveFile)-3] + "png"
+	if err := imaging.Save(covelImage, covelName); err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
+
+	//在Videos数据库中插入数据
+	CreateVideoErr := db.Create(&Video{AuthorID: user.Id, PlayUrl: videoName, CoverUrl: videoName[:len(videoName)-3] + "png"}).Error
+	if CreateVideoErr != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  CreateVideoErr.Error(),
 		})
 		return
 	}
