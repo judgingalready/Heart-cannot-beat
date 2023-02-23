62dba6cd678adf999ac1d2c13a96fdcb58623c40
shushengyuan <2637975471@qq.com>
2023-02-23 11:59:20 +0800

    bugfix: 修复feed无法显示已经点赞的bug

--- controller/feed.go
+++ controller/feed.go
@@ -3,6 +3,7 @@ package controller
 import (
 	// "fmt"
 
+	"fmt"
 	"net/http"
 	"time"
 
@@ -17,8 +18,33 @@ type FeedResponse struct {
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
+	token := c.Query("token")
+	var user User
+	verifyErr := VerifyToken(token, &user)
+	if verifyErr != nil {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "tokenis invalid"},
+		})
+		return
+	}
+
 	var videos []Video
 	SearchVideoForFeed(&videos)
+	for i, _ := range videos {
+		var count int64
+		db.Model(&Like{}).
+			Where("user_id = ? AND video_id = ?", user.Id, videos[i].Id).
+			Count(&count)
+		fmt.Println("count %d", count)
+		if count > 0 {
+			// 视频已被点过赞
+			videos[i].IsFavorite = true
+		} else {
+			// 视频未被点过赞
+			videos[i].IsFavorite = false
+		}
+	}
+
 	c.JSON(http.StatusOK, FeedResponse{
 		Response:  Response{StatusCode: 0},
 		VideoList: videos,
