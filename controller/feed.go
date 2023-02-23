02da0617b7fba9d4a4263d835501e572059da6b7
shushengyuan <2637975471@qq.com>
2023-02-23 12:27:49 +0800

    bugfix: feed流倒序，并且不晚于last_time

--- controller/feed.go
+++ controller/feed.go
@@ -5,6 +5,7 @@ import (
 
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +20,16 @@ type FeedResponse struct {
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
 	token := c.Query("token")
+	latestTime, lastTimeErr := strconv.ParseInt(c.Query("latest_time"), 10, 64)
+	if lastTimeErr != nil {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "video_id is invalid"},
+		})
+		return
+	}
+
+	fmt.Println(latestTime)
+
 	var user User
 	verifyErr := VerifyToken(token, &user)
 	if verifyErr != nil {
@@ -29,13 +40,12 @@ func Feed(c *gin.Context) {
 	}
 
 	var videos []Video
-	SearchVideoForFeed(&videos)
+	SearchVideoForFeed(&videos, latestTime)
 	for i, _ := range videos {
 		var count int64
 		db.Model(&Like{}).
 			Where("user_id = ? AND video_id = ?", user.Id, videos[i].Id).
 			Count(&count)
-		fmt.Println("count %d", count)
 		if count > 0 {
 			// 视频已被点过赞
 			videos[i].IsFavorite = true
